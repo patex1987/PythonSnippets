40f105d7660c3b018b6ed59b67fecc05f4b002b6
patex1987 <patex1987@home_PC>
2017-07-01 09:25:21 +0200

    MainSolver - Maze printing added

--- MazeSolver/MainSolver.py
+++ MazeSolver/MainSolver.py
@@ -1,8 +1,43 @@
 '''
-Created on 1. 7. 2017
+Created on 28. 6. 2017
 
 @author: patex1987
 '''
+import os
+import MazeException
+
+
+def cls():
+    '''
+    Clears the terminal window
+    Should work on windows and Linux as well
+    '''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def print_maze(maze_2d, fill_length=1):
+    '''
+    Prints the maze in a pretty form
+    '''
+    if len(maze_2d) < 2:
+        raise MazeException.ImproperMaze
+    if len(maze_2d[0]) < 1:
+        raise MazeException.ImproperMaze
+    base_width = len(maze_2d)
+    line_width = ((1+(2*(fill_length))+1)*base_width) + 1
+    print('-'*line_width)
+    basic_elem = '{fill}|{fill}'.format(fill=' '*fill_length)
+    for line in maze_2d:
+        line_text = basic_elem.join((str(elem) for elem in line))
+        print('|{fill}{data}{fill}|'.format(fill=' '*fill_length,
+                                            data=line_text))
+        print('-'*line_width)
+
 
 if __name__ == '__main__':
-    pass
+    MAZE = [[0, 0, 0, 0, 0, 1],
+            [1, 1, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0, 1],
+            [0, 1, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 2]]
