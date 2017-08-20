c26f2218fc562ec68bc32311a3ef12fbd9db888a
Patex <Patex@Patex-PC>
2017-08-20 22:05:39 +0200

    Knowledge update - Tkinter

--- Knowledge_log.py
+++ Knowledge_log.py
@@ -311,3 +311,18 @@ class SlottedWithDict(Child):
 
 # How to make compound labels in Tkinter
 # Compund - overlaying image and text
+
+from Tkinter import *
+
+root = Tk()
+logo = PhotoImage(file="../images/python_logo_small.gif")
+explanation = """At present, only GIF and PPM/PGM
+formats are supported, but an interface 
+exists to allow additional image file
+formats to be added easily."""
+w = Label(root, 
+          compound = CENTER,
+          text=explanation, 
+          image=logo).pack(side="right")
+
+root.mainloop()
